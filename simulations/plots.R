5d6d091bb7e47e98f9d897b3656f6c7398a3624f
Bastian <bastianxpfeifer@gmail.com>
2025-02-06 12:46:10 +0100

    continued ...

--- simulations/plots.R
+++ simulations/plots.R
@@ -1,12 +1,20 @@
 
+
+#### DATASETS AUC ####
+setwd("~/GitHub/cpath/SIM_DATASETS_auc/")
 library(reshape)
 library(ggplot2)
 
 L = list()
+NN = c("SHAP","LIME","CPATH","CPATH_min")
 
 L[[1]] = read.table("BREAST_auc.txt")
 L[[2]] = read.table("DIABETES_auc.txt")
 L[[3]] = read.table("IONOSPHERE_auc.txt")
+
+colnames(L[[1]]) = NN
+colnames(L[[2]]) = NN
+colnames(L[[3]]) = NN
 
 names(L) = c("Breast","Diabetes","Ionosphere")
 
@@ -16,6 +24,113 @@ p = ggplot(L_melt, aes(x=variable, y=value)) +
   geom_boxplot(notch=TRUE) +
   #facet_wrap(. ~ variable, scales="free")
   #facet_grid(cols = vars(L1), scales = "free_y")
-  theme(text = element_text(size=20))+
-  facet_grid(L1~., scales = "free")
-  
+  ylab("AUC")+
+  xlab("Methods") +
+  theme(text = element_text(size=15)) +
+  facet_grid(L1~., scales = "free") 
+##########################################
+
+## CPATH correlation with CPATH_min
+setwd("~/GitHub/cpath/SIM_CPATH_corr_CPATH_min/")
+library(reshape)
+library(ggplot2)
+
+L = list()
+L[[1]] = read.table("COR_sim.txt")
+L[[2]] = read.table("COR_sim3.txt")
+L[[3]] = read.table("COR_sim4.txt")
+L[[4]] = read.table("COR_sim5.txt")
+
+NN = c("Conditional dependency (1)",
+      "Correlation","Conditional in-dependency",
+      "Conditional dependency (2)")
+
+names(L) = NN
+
+L_melt = melt(L)
+
+p = ggplot(L_melt, aes(x=variable, y=value)) + 
+  geom_boxplot(notch=FALSE) +
+  #facet_wrap(. ~ variable, scales="free")
+  #facet_grid(cols = vars(L1), scales = "free_y")
+  ylab("CPATH Correlation with CPATH_min")+
+  xlab("") +
+  theme(text = element_text(size=15)) +
+  facet_wrap(~L1,ncol=2) 
+##########################################
+
+
+setwd("~/GitHub/cpath/SIM_DATASETS_corr_with_gini/")
+library(ggplot2)
+library(reshape)
+
+NN = c("SHAP","LIME","CPATH","CPATH_min")
+NN2 = c("Breast","Diabetes","Ionosphere","Iris")
+
+
+L = list()
+L[[1]] = read.table("BREAST_corr_with_gini.txt")
+L[[2]] = read.table("DIABETES_corr_with_gini.txt")
+L[[3]] = read.table("IONOSPHERE_corr_with_gini.txt")
+L[[4]] = read.table("IRIS_corr_with_gini.txt")
+
+
+
+L[[1]] = cbind(read.table("SHAP_BREAST_corr_with_gini.txt")[,1],L[[1]])
+L[[2]] = cbind(read.table("SHAP_DIABETES_corr_with_gini.txt")[,1],L[[2]])
+L[[3]] = cbind(read.table("SHAP_IONOSPHERE_corr_with_gini.txt")[,1],L[[3]])
+L[[4]] = cbind(read.table("SHAP_IRIS_corr_with_gini.txt")[,1],L[[4]])
+
+colnames(L[[1]]) = NN
+colnames(L[[2]]) = NN
+colnames(L[[3]]) = NN
+colnames(L[[4]]) = NN
+
+names(L) = NN2
+
+L_melt = melt(L)
+
+p = ggplot(L_melt, aes(x=variable, y=value)) + 
+  geom_boxplot(notch=FALSE) +
+  #facet_wrap(. ~ variable, scales="free")
+  #facet_grid(cols = vars(L1), scales = "free_y")
+  ylab("Spearman Correlation wih Gini")+
+  xlab("") +
+  theme(text = element_text(size=15)) +
+  facet_wrap(~L1,ncol=2) 
+
+##############################################################
+
+
+setwd("~/GitHub/cpath/SIM_DATASETS_corr_with_treeSHAP/")
+library(ggplot2)
+library(reshape)
+
+NN = c("SHAP","LIME","CPATH","CPATH_min")
+NN2 = c("Breast","Diabetes","Ionosphere","Iris")
+
+
+L = list()
+L[[1]] = read.table("SIM_BREAST_corr_with_treeSHAP.txt")
+L[[2]] = read.table("SIM_DIABETES_corr_with_treeSHAP.txt")
+L[[3]] = read.table("SIM_IONOSPHERE_corr_with_treeSHAP.txt")
+L[[4]] = read.table("SIM_IRIS_corr_with_treeSHAP.txt")
+
+
+colnames(L[[1]]) = NN
+colnames(L[[2]]) = NN
+colnames(L[[3]]) = NN
+colnames(L[[4]]) = NN
+
+names(L) = NN2
+
+L_melt = melt(L)
+
+p = ggplot(L_melt, aes(x=variable, y=value)) + 
+  geom_boxplot(notch=FALSE) +
+  #facet_wrap(. ~ variable, scales="free")
+  #facet_grid(cols = vars(L1), scales = "free_y")
+  ylab("Spearman Correlation wih TreeShap")+
+  xlab("") +
+  theme(text = element_text(size=15)) +
+  facet_wrap(~L1,ncol=2) 
---
+++ simulations/simulation_cpath_speed.R
@@ -0,0 +1,94 @@
+# main 
+library(ranger)
+library(lime)
+library(cpath)
+library(shapr)
+
+n.sim=50
+feats = c(10, 50, 100, 1000)
+RES = matrix(NaN, n.sim, length(feats))
+colnames(RES) = feats
+RES_speed = RES
+
+
+for(xx in 1:length(feats)){
+for(ii in 1:n.sim){
+
+cat(ii, " of ", n.sim, "\n")
+
+#res  = sim5(feats[xx])# conditional dependency (2)
+#res  = sim(feats[xx])# conditional dependency (1)
+#res  = sim3(feats[xx])# correlation
+res  = sim4(feats[xx])# conditional in-dependency
+
+
+
+data = res$data
+target = res$target
+
+while(TRUE){
+
+    model = ranger(x=data,y=as.factor(target), 
+                num.trees=100, 
+                classification=TRUE, 
+                probability=TRUE, 
+                #mtry=4, 
+                #replace=TRUE,#), 
+                importance='impurity')
+    pred = predict(model, data)$predictions
+    pred = apply(pred,1,function(x){which.max(x)-1})
+
+    #print("?")
+    if(all(pred==0)|all(pred==1)){
+        next
+    #}
+
+    #if(ModelMetrics::auc(pred, target)<0.70){
+        
+    #    print("Accuracy too low!")
+    #    rm(.Random.seed, envir=globalenv())
+    #    Sys.sleep(2)
+    #    next
+
+    }else{break}
+}
+
+print("Model Accuracy")
+print(ModelMetrics::auc(pred, target))
+
+speed = system.time({
+## CPATH_min 
+# Get the counterfactual paths
+P   = cpath::cpaths_mc(model, data, k=4, n_paths= 10000, nearest=FALSE)
+if(sum(P$counterfactuality)<=2){
+    RES_speed[ii,xx] = speed[3]
+    RES[ii,xx] = 0
+    print(RES)
+    print(RES_speed)
+    next
+}
+
+# Build transition matrix 
+T   = cpath::transition(P)
+
+# Get global feature importances
+IMP2 = cpath::importance(T)
+})
+
+#print(speed)
+RES_speed[ii,xx] = speed[3]
+
+# Get coverage
+names(IMP2) = 1:length(IMP2)
+IMP2_sorted = sort(IMP2, decreasing=TRUE)
+
+cov_cpath_min = sum(is.element(names(IMP2_sorted)[1:2],1:2))/2
+
+
+RES[ii,xx] = cov_cpath_min
+
+print(RES)
+print(RES_speed)
+
+} # End of simulation
+}
